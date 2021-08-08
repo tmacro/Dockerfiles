afd8c46f6669edc30727f4b07e21be3998503a1b
Taylor McKinnon <mail@tmacs.space>
2021-08-08 14:53:33 -0700

    initial code

---
+++ promhealth/exporter.js
@@ -0,0 +1,41 @@
+const { createServer } = require("http");
+
+const client = require('prom-client');
+const bent = require('bent')
+
+const targetURL = process.env.TARGET_URL || 'http://localhost/';
+const targetMethod = process.env.TARGET_METHOD ||  'GET';
+const expectedCode = parseInt(process.env.TARGET_CODE || '200');
+
+const checkHealth = bent(targetURL, targetMethod, 'string');
+
+const healthGauge = new client.Gauge({
+  name: 'up',
+  help: 'Did the healthcheck pass. Binary 0/1',
+  async collect() {
+    let passed = false;
+
+    try {
+        await checkHealth();
+        passed = true;
+    } catch (error) {
+        console.log(error)
+        if (error.statusCode === expectedCode) {
+            passed = true;
+        }
+    }
+
+    this.set(passed ? 1 : 0);
+  },
+});
+
+createServer((req, res) => {
+  switch (req.url) {
+    case "/metrics":
+        client.register.metrics().then(output => res.end(output))
+        break;
+    default:
+        res.statusCode = 404;
+        res.end("Page not found!");
+  }
+}).listen(3000);
