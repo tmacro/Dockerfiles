67b9821dd84b3386f93304f697c938a39ff18dff
Taylor McKinnon <mail@tmacs.space>
2022-11-05 14:19:54 -0700

    add rclone to aria2-rpc image along with event hook environment variables

--- images/aria2-rpc/entrypoint.sh
+++ images/aria2-rpc/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -n "$ON_START" ]; then
+    sh -c "$ON_START"
+fi
+
 FLAGS="-d $DOWNLOAD_DIR
     -j $PARALLEL_DOWNLOADS
     --enable-rpc
@@ -8,7 +12,20 @@ FLAGS="-d $DOWNLOAD_DIR
     --rpc-secret $RPC_SECRET
     --disable-ipv6
     --rpc-allow-origin-all
+    --always-resume
     --show-console-readout false $@"
+
+if [ -n "$ON_DOWNLOAD_START" ]; then
+    FLAGS="$FLAGS --on-download-start $ON_DOWNLOAD_START"
+fi
+
+if [ -n "$ON_DOWNLOAD_COMPLETE" ]; then
+    FLAGS="$FLAGS --on-download-complete $ON_DOWNLOAD_COMPLETE"
+fi
+
+if [ -n "$ON_DOWNLOAD_ERROR" ]; then
+    FLAGS="$FLAGS --on-download-error $ON_DOWNLOAD_ERROR"
+fi
 
 echo $FLAGS
 
